94a9cee1229470aa210f351ebc5b312f404c3948
Saiful Islam <saif_lesnar@outlook.com>
2022-05-09 15:01:19 +0600

    Commands for creating a team and player

---
+++ src/FantasyTeams.WebService/Commands/CreateNewPlayerCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace FantasyTeams.Commands
+{
+    public class CreateNewPlayerCommand
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ src/FantasyTeams.WebService/Commands/CreateNewTeamCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace FantasyTeams.Commands
+{
+    public class CreateNewTeamCommand
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+    }
+}
