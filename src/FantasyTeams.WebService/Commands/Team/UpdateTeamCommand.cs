2594616957ac7fb7ef43cea47ae2c28573037139
Saiful Islam <saif_lesnar@outlook.com>
2022-05-13 06:48:01 +0600

    Command updated with validation

--- src/FantasyTeams.WebService/Commands/Team/UpdateTeamCommand.cs
+++ src/FantasyTeams.WebService/Commands/Team/UpdateTeamCommand.cs
@@ -1,10 +1,12 @@
 ﻿using FantasyTeams.Models;
 using MediatR;
+using System.ComponentModel.DataAnnotations;
 
 namespace FantasyTeams.Commands
 {
     public class UpdateTeamCommand : IRequest<CommandResponse>
     {
+        [Required]
         public string TeamId { get; set; }
         public string Country { get; set; }
         public string Name { get; set; }
