bf3b648e78d6eee7c1ea47b54dc26d6b2a27fbfb
Saiful Islam <saif_lesnar@outlook.com>
2022-05-13 06:07:52 +0600

    User login Command validation added

--- src/FantasyTeams.WebService/Commands/UserLoginCommand.cs
+++ src/FantasyTeams.WebService/Commands/UserLoginCommand.cs
@@ -1,11 +1,17 @@
 ﻿using FantasyTeams.Models;
 using MediatR;
+using System.ComponentModel.DataAnnotations;
 
 namespace FantasyTeams.Commands
 {
     public class UserLoginCommand : IRequest<AuthCommandResponse>
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,10}$", 
+            ErrorMessage = "At least one upper case English letter, one lower case English letter, one digit, one special character and length of 8 to 10")]
         public string Password { get; set; }
     }
 }
