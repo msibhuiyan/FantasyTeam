953843135410c35bc7ffc01e906e72a2001dacd8
Saiful Islam <saif_lesnar@outlook.com>
2022-05-13 11:31:41 +0600

    Uam validation added

--- src/FantasyTeams.WebService/Commands/Uam/DeleteUserCommand.cs
+++ src/FantasyTeams.WebService/Commands/Uam/DeleteUserCommand.cs
@@ -1,11 +1,14 @@
 ﻿using FantasyTeams.Models;
 using MediatR;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 
 namespace FantasyTeams.Commands
 {
     public class DeleteUserCommand : IRequest<CommandResponse>
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
     }
 }
