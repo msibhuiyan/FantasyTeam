d54f2c1235cac15a315a07562e77a902f7e9c7eb
Saiful Islam <saif_lesnar@outlook.com>
2022-05-13 16:29:41 +0600

    Query handler added

---
+++ src/FantasyTeams.WebService/QueryHandler/Player/GetAllPlayerQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using FantasyTeams.Contracts;
+using FantasyTeams.Models;
+using FantasyTeams.Queries;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FantasyTeams.QueryHandler.Player
+{
+    public class GetAllPlayerQueryHandler : IRequestHandler<GetAllPlayerQuery, QueryResponse>
+    {
+        private readonly IPlayerService _playerService;
+        public GetAllPlayerQueryHandler(IPlayerService playerService)
+        {
+            _playerService = playerService;
+        }
+
+        public async Task<QueryResponse> Handle(GetAllPlayerQuery request, CancellationToken cancellationToken)
+        {
+            return await _playerService.GetAllPlayer();
+        }
+    }
+}
