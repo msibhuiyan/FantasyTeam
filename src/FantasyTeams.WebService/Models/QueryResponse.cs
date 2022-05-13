59b870523d1d249863199e44a53a13710f1a146e
Saiful Islam <saif_lesnar@outlook.com>
2022-05-13 09:10:44 +0600

    new query response added

---
+++ src/FantasyTeams.WebService/Models/QueryResponse.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FantasyTeams.Models
+{
+    public class QueryResponse
+    {
+        internal QueryResponse(bool succeeded, object results, IEnumerable<string> errors)
+        {
+            Succeeded = succeeded;
+            Results = results;
+            Errors = errors.ToArray();
+        }
+        public bool Succeeded { get; set; }
+
+        public string[] Errors { get; set; }
+        public object Results { get; set; }
+
+        public static QueryResponse Success(object results)
+        {
+            return new QueryResponse(true, results, new string[] { });
+        }
+        public static QueryResponse Success()
+        {
+            return new QueryResponse(true, new string[] { }, new string[] { });
+        }
+
+        public static QueryResponse Failure(IEnumerable<string> errors)
+        {
+            return new QueryResponse(false, new string[] { }, errors);
+        }
+    }
+}
