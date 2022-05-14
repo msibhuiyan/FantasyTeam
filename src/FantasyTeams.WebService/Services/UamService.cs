ed0874ef7d49feadd6657582d76dc7c3f1866d9e
Saiful Islam <saif_lesnar@outlook.com>
2022-05-14 21:27:20 +0600

    Uam refactored

--- src/FantasyTeams.WebService/Services/UamService.cs
+++ src/FantasyTeams.WebService/Services/UamService.cs
@@ -14,6 +14,7 @@ using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace FantasyTeams.Services
 {
@@ -21,40 +22,43 @@ namespace FantasyTeams.Services
     {
         private readonly ILogger<UamService> _logger;
         private readonly IConfiguration _configuration;
-        private readonly IUserRepository _repository;
-        private readonly ITeamService _teamService;
+        private readonly IUserRepository _userRepository;
+        private readonly ITeamRepository _teamRepository;
+        private readonly IPlayerRepository _playerRepository;
 
         public UamService(ILogger<UamService> logger,
             IConfiguration configuration,
-            IUserRepository repository,
-            ITeamService teamService)
+            IUserRepository userRepository,
+            ITeamRepository teamRepository,
+            IPlayerRepository playerRepository)
         {
             _logger = logger;
             _configuration = configuration;
-            _repository = repository;
-            _teamService = teamService;
+            _userRepository = userRepository;
+            _teamRepository = teamRepository;
+            _playerRepository = playerRepository;
         }
 
         public async Task<CommandResponse> DeleteUser(DeleteUserCommand deleteUserCommand)
         {
-            var user = await _repository.GetByEmailAsync(deleteUserCommand.Email);
+            var user = await _userRepository.GetByEmailAsync(deleteUserCommand.Email);
             if(user == null)
             {
                 return CommandResponse.Failure(new string[] { "User doesn't exists for deletion" });
             }
-            await _teamService.DeleteTeam(user.TeamId);
-            await _repository.DeleteAsync(deleteUserCommand.Email);
+            await _teamRepository.DeleteAsync(user.TeamId);
+            await _userRepository.DeleteAsync(deleteUserCommand.Email);
             return CommandResponse.Success();
         }
 
         public async Task<User> GetUserInfo(string userEmail)
         {
-            return await _repository.GetByEmailAsync(userEmail);
+            return await _userRepository.GetByEmailAsync(userEmail);
         }
 
         public async Task<CommandResponse> RegisterUser(UserRegistrationCommand userRegistrationCommand)
         {
-            var user = await _repository.GetByEmailAsync(userRegistrationCommand.Email);
+            var user = await _userRepository.GetByEmailAsync(userRegistrationCommand.Email);
             if (user != null)
             {
                 return CommandResponse.Failure(new string[] { "User Already Exists" });
@@ -74,25 +78,153 @@ namespace FantasyTeams.Services
             user.LastName = userRegistrationCommand.LastName;
             user.Country = userRegistrationCommand.Country;
             user.Role = Role.Member.ToString();
-            var teamCreationResponse = await _teamService.CreateNewTeam(new CreateTeamCommand{
-                Name = user.TeamName,
-                Country = user.Country
-            }, user.TeamId);
+            var teamCreationResponse = await CreateNewTeam(user.TeamName, user.Country,
+             user.TeamId);
             if(teamCreationResponse.Errors.Length > 0)
             {
                 var userCreationResponseErrors = teamCreationResponse.Errors.ToList();
                 userCreationResponseErrors.Add("User creation failed");
                 return CommandResponse.Failure(userCreationResponseErrors.ToArray());
             }
-            await _repository.CreateAsync(user);
+            await _userRepository.CreateAsync(user);
 
             return CommandResponse.Success();
+        }
+        public async Task<CommandResponse> CreateNewTeam(string teamName, string country, string teamId)
+        {
+            var team = await _teamRepository.GetByNameAsync(teamName);
+            if (team != null)
+            {
+                return CommandResponse.Failure(new string[] { "Team already exists." });
+            }
+            team = new Team();
+            team.Id = teamId;
+            team.Name = teamName;
+            team.Country = country;
+
+            var getTeamMembers = await CreateNewTeamPlayers(team);
+
+            team.Attackers = getTeamMembers.Where(x => x.PlayerType == PlayerType.Attacker.ToString()).Select(x => x.Id).ToArray();
+            team.Defenders = getTeamMembers.Where(x => x.PlayerType == PlayerType.Defender.ToString()).Select(x => x.Id).ToArray();
+            team.MidFielders = getTeamMembers.Where(x => x.PlayerType == PlayerType.MidFielder.ToString()).Select(x => x.Id).ToArray();
+            team.GoalKeepers = getTeamMembers.Where(x => x.PlayerType == PlayerType.GoalKeeper.ToString()).Select(x => x.Id).ToArray();
+            team.Budget = 5000000;
+            team.Value = 20000000;
+            await _teamRepository.CreateAsync(team);
+            return CommandResponse.Success();
+        }
+
+        public async Task<List<Player>> CreateNewTeamPlayers(Team team)
+        {
+            List<Player> players = new List<Player>();
+            players.AddRange(AddGoalKeepers(team));
+            players.AddRange(AddDefenders(team));
+            players.AddRange(AddAttackers(team));
+            players.AddRange(AddMidFielders(team));
+
+            await _playerRepository.CreateManyAsync(players);
+            return players;
+        }
+        private IEnumerable<Player> AddMidFielders(Team team)
+        {
+            Random rnd = new Random();
+            List<Player> players = new List<Player>();
+            for (int i = 0; i < 6; i++)
+            {
+                var player = new Player();
+                player.Id = Guid.NewGuid().ToString();
+                player.FirstName = "MidFielder";
+                player.LastName = Guid.NewGuid().ToString();
+                player.FullName = player.FirstName + " " + player.LastName;
+                player.Country = team.Country;
+                player.Value = 1000000;
+                player.Age = rnd.Next(18, 40);
+                player.ForSale = false;
+                player.AskingPrice = 0;
+                player.PlayerType = PlayerType.MidFielder.ToString();
+                player.TeamId = team.Id;
+                player.TeamName = team.Name;
+                players.Add(player);
+            }
+            return players;
+        }
+
+        private IEnumerable<Player> AddAttackers(Team team)
+        {
+            Random rnd = new Random();
+            List<Player> players = new List<Player>();
+            for (int i = 0; i < 5; i++)
+            {
+                var player = new Player();
+                player.Id = Guid.NewGuid().ToString();
+                player.FirstName = "Attacker";
+                player.LastName = Guid.NewGuid().ToString();
+                player.FullName = player.FirstName + " " + player.LastName;
+                player.Country = team.Country;
+                player.Value = 1000000;
+                player.Age = rnd.Next(18, 40);
+                player.ForSale = false;
+                player.AskingPrice = 0;
+                player.PlayerType = PlayerType.Attacker.ToString();
+                player.TeamId = team.Id;
+                player.TeamName = team.Name;
+                players.Add(player);
+            }
+            return players;
+        }
+
+        private IEnumerable<Player> AddDefenders(Team team)
+        {
+            Random rnd = new Random();
+            List<Player> players = new List<Player>();
+            for (int i = 0; i < 6; i++)
+            {
+                var player = new Player();
+                player.Id = Guid.NewGuid().ToString();
+                player.FirstName = "Defender";
+                player.LastName = Guid.NewGuid().ToString();
+                player.FullName = player.FirstName + " " + player.LastName;
+                player.Country = team.Country;
+                player.Value = 1000000;
+                player.Age = rnd.Next(18, 40);
+                player.ForSale = false;
+                player.AskingPrice = 0;
+                player.PlayerType = PlayerType.Defender.ToString();
+                player.TeamId = team.Id;
+                player.TeamName = team.Name;
+                players.Add(player);
+            }
+            return players;
+        }
+
+        private IEnumerable<Player> AddGoalKeepers(Team team)
+        {
+            Random rnd = new Random();
+            List<Player> players = new List<Player>();
+            for (int i = 0; i < 3; i++)
+            {
+                var player = new Player();
+                player.Id = Guid.NewGuid().ToString();
+                player.FirstName = "Keeper";
+                player.LastName = Guid.NewGuid().ToString();
+                player.FullName = player.FirstName + " " + player.LastName;
+                player.Country = team.Country;
+                player.Value = 1000000;
+                player.Age = rnd.Next(18, 40);
+                player.ForSale = false;
+                player.AskingPrice = 0;
+                player.PlayerType = PlayerType.GoalKeeper.ToString();
+                player.TeamId = team.Id;
+                player.TeamName = team.Name;
+                players.Add(player);
+            }
+            return players;
         }
 
         public async Task<CommandResponse> UserLogin(UserLoginCommand userLoginCommand)
         {
             
-            var user = await _repository.GetByEmailAsync(userLoginCommand.Email);
+            var user = await _userRepository.GetByEmailAsync(userLoginCommand.Email);
             if (user == null)
                 return CommandResponse.Failure(new string[] { "User Doesn't Exists" });
             if (!VerifyPasswordHash(userLoginCommand.Password, user.Password, user.Salt))
