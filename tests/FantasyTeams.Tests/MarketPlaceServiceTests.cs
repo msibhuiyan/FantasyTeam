00591b9c7a66714e5fde26d52763517ba6f59898
Saiful Islam <saif_lesnar@outlook.com>
2022-05-14 22:39:47 +0600

    test initialization

--- tests/FantasyTeams.Tests/MarketPlaceServiceTests.cs
+++ tests/FantasyTeams.Tests/MarketPlaceServiceTests.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using AutoFixture;
+using FantasyTeams.Contracts;
+using FantasyTeams.Repository;
+using FantasyTeams.Services;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +12,20 @@ namespace FantasyTeams.Tests
 {
     public class MarketPlaceServiceTests
     {
+        private readonly Mock<ILogger<MarketPlaceService>> _logger;
+        private readonly Mock<ITeamRepository> _teamRepository;
+        private readonly Mock<IMarketPlaceRepository> _marketRepository;
+        private readonly IMarketPlaceService _sut;
+        private readonly IFixture _fixture = new Fixture();
+        public MarketPlaceServiceTests()
+        {
+            _logger = new Mock<ILogger<MarketPlaceService>>();
+            _teamRepository = new Mock<ITeamRepository>();
+            _marketRepository = new Mock<IMarketPlaceRepository>();
+            _sut = new MarketPlaceService(
+                _logger.Object,
+                _marketRepository.Object,
+                _teamRepository.Object);
+        }
     }
 }
--- tests/FantasyTeams.Tests/PlayerServiceTests.cs
+++ tests/FantasyTeams.Tests/PlayerServiceTests.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿using AutoFixture;
+using FantasyTeams.Contracts;
+using FantasyTeams.Repository;
+using FantasyTeams.Services;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace FantasyTeams.Tests
 {
+
     public class PlayerServiceTests
     {
+        private readonly Mock<ILogger<PlayerService>> _logger;
+        private readonly Mock<ITeamRepository> _teamRepository;
+        private readonly Mock<IPlayerRepository> _playerRepository;
+        private readonly IPlayerService _sut;
+        private readonly IFixture _fixture = new Fixture();
+        public PlayerServiceTests()
+        {
+            _logger = new Mock<ILogger<PlayerService>>();
+            _teamRepository = new Mock<ITeamRepository>();
+            _playerRepository = new Mock<IPlayerRepository>();
+            _sut = new PlayerService(
+                _logger.Object,
+                _playerRepository.Object,
+                _teamRepository.Object);
+        }
     }
 }
