3b63479001c4faea551f94e9a5c206c0e61036b5
Saiful Islam <saif_lesnar@outlook.com>
2022-05-15 15:40:07 +0600

    Data validation for assigning team

--- src/FantasyTeams.WebService/Commands/Team/AssignTeamCommand.cs
+++ src/FantasyTeams.WebService/Commands/Team/AssignTeamCommand.cs
@@ -1,11 +1,18 @@
 ﻿using FantasyTeams.Models;
 using MediatR;
+using System.ComponentModel.DataAnnotations;
 
 namespace FantasyTeams.Commands.Team
 {
     public class AssignTeamCommand : IRequest<CommandResponse>
     {
+        [Required]
+        [RegularExpression("[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}",
+            ErrorMessage = "Please provide correct GUID")]
         public string UserId { get; set; }
+        [Required]
+        [RegularExpression("[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}",
+            ErrorMessage = "Please provide correct GUID")]
         public string TeamId { get; set; }
     }
 }
